d04bc2122d8ed6749114e4e5a5c38f75c07a20b9
FatAnorexic <54606095+FatAnorexic@users.noreply.github.com>
2023-10-30 14:24:33 -0400

    Add for loop creating a series of empty div containers
    The for loop takes the size variable which is initialzed to 256(16 x 16) if it is
    left blank by the resize button(to be added later). The function call at the bottom
    of etch_a_script.js is simply a test to be removed later

--- js/etch_a_script.js
+++ js/etch_a_script.js
@@ -1,5 +1,13 @@
 
 function gridDiv(size=256){
     console.log(size);
+
+    const container = document.querySelector('#container');
+    for(let x=0;x<size;x++){
+        const cell=document.createElement('div');
+        cell.classList.add('cell');
+        container.appendChild(cell);
+    }
 }
 
+gridDiv()
