8d83e2585dafed35fc4c64471f9149c928d98d90
FatAnorexic <54606095+FatAnorexic@users.noreply.github.com>
2023-10-30 15:30:50 -0400

    Add test to basic dim for style elements in for loop

--- js/etch_a_script.js
+++ js/etch_a_script.js
@@ -6,6 +6,10 @@ function gridDiv(size=256){
     for(let x=0;x<size;x++){
         const cell=document.createElement('div');
         cell.classList.add('cell');
+        cell.style.width="10px";
+        cell.style.height="10px";
+        cell.style.color="black";
+        cell.style.outline="inset"
         container.appendChild(cell);
     }
 }
