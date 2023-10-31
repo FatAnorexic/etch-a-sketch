d587cda891d38dd32ccb0949d2e8ea564db4404a
FatAnorexic <54606095+FatAnorexic@users.noreply.github.com>
2023-10-31 14:22:06 -0400

    Comment out draw function
    Need to possibly change the nested for loop to a single loop that iterates once

--- js/etch_a_script.js
+++ js/etch_a_script.js
@@ -2,22 +2,31 @@
 function gridDiv(row=16, column=16){
     size=row*column;
     console.log(size);
-
+    let thing=true;
     const container = document.querySelector('#container');
     for(let x=0;x<row;x++){
         const r=document.createElement('div');
-        r.classList.add('row');
+        r.id='row';
         container.appendChild(r);
         for(let y=0;y<column;y++){
             const c=document.createElement('div');
-            c.classList.add('column');
+            c.id='column';
             r.appendChild(c);
         }
     }
+    draw(size);
 }
 
-function draw(){
-}
 //Remove me later[for testing purposes]
 gridDiv();
-draw();
+
+/*
+function draw(size){
+    for(let x=0;x<size;x++){
+        let pen=document.getElementById('column');
+        pen.addEventListener('mouseenter', ()=>{
+            pen.style.background='black';
+        })
+    }
+}
+*/
