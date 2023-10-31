cc590f7d9877d55919cfff018c21b6556bd20173
FatAnorexic <54606095+FatAnorexic@users.noreply.github.com>
2023-10-31 15:31:56 -0400

    Add rudementry code allowing cells to be colored
    Added to the function draw. For each cell in the node array, if the mouse enters,
    the cell is colored black

--- js/etch_a_script.js
+++ js/etch_a_script.js
@@ -13,13 +13,19 @@ function gridDiv(row=16, column=16){
             r.appendChild(c);
         }
     }
-    draw(size);
 }
 
 //Remove me later[for testing purposes]
 gridDiv();
 
 
-function draw(size){
-    
+function draw(){
+    let targets=document.querySelectorAll('.column');
+    console.log(targets);
+    targets.forEach((target)=>{
+        target.addEventListener('mouseenter', ()=>{
+            target.style.background='black';
+        })
+    })
 }
+draw();
