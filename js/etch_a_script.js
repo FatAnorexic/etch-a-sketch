8413cd2116978f77773eb0636da598deb602a501
FatAnorexic <54606095+FatAnorexic@users.noreply.github.com>
2023-10-31 14:56:27 -0400

    Remove row class declarations and styles

--- js/etch_a_script.js
+++ js/etch_a_script.js
@@ -2,15 +2,14 @@
 function gridDiv(row=16, column=16){
     size=row*column;
     console.log(size);
-    let thing=true;
     const container = document.querySelector('#container');
     for(let x=0;x<row;x++){
         const r=document.createElement('div');
-        r.id='row';
+        // r.className='row';
         container.appendChild(r);
         for(let y=0;y<column;y++){
             const c=document.createElement('div');
-            c.id='column';
+            c.className='column';
             r.appendChild(c);
         }
     }
@@ -20,13 +19,7 @@ function gridDiv(row=16, column=16){
 //Remove me later[for testing purposes]
 gridDiv();
 
-/*
+
 function draw(size){
-    for(let x=0;x<size;x++){
-        let pen=document.getElementById('column');
-        pen.addEventListener('mouseenter', ()=>{
-            pen.style.background='black';
-        })
-    }
+    
 }
-*/
